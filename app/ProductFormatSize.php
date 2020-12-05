9e9b231787cb78a76854a53d611475f0b046589e
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-12-04 21:27:04 -0400

    cambios

--- app/ProductFormatSize.php
+++ app/ProductFormatSize.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductFormatSize extends Model
 {
+
+    use SoftDeletes;
     
     public function product(){
         return $this->belongsTo(Product::class);
