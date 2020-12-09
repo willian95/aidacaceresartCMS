c6bf4f3bdde47ed7db20e3edd99239c1bb3d46fe
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-12-09 11:52:30 -0400

    cambios

--- app/Format.php
+++ app/Format.php
@@ -3,12 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Format extends Model
 {
-
-    use SoftDeletes;
 
     public function productFormatSizes(){
         return $this->hasMany(ProductFormatSize::class);
